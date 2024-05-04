d1ddf28bc54a66dae7464d78664a2cf72f9f63cd
Ryan <ryannolanco@gmail.com>
2024-05-04 11:30:18 -0600

    minor change

--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -1,7 +1,8 @@
 import CardForm from "./CardForm"
+import { readCard } from "../utils/api"
+import { useParams } from "react-router-dom"
 
 const EditCard = ( {foundDeck, setCardFormData, cardFormData, intialCardFormData }) => {
-
 
   return (
     <>{<CardForm
