6411b9ffc8fb4de7603a5290ce31f692df4d2449
Ryan <ryannolanco@gmail.com>
2024-05-04 11:29:09 -0600

    completed new card and update card form logic

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { createCard } from "../utils/api";
+import { createCard, updateCard, readCard } from "../utils/api";
+import { useEffect } from "react";
 
 
 
@@ -7,7 +8,39 @@ const CardForm = ({ cardFormData, setCardFormData, foundDeck, intialCardFormData
   const navigate = useNavigate();
   const { cardId, deckId } = useParams();
   const deckCards = foundDeck.cards
-  
+
+
+
+  useEffect(() => {
+    if (cardId) {
+      async function retrieveCardData(cardId) {
+        const controller = new AbortController();
+        const { signal } = controller
+        try {
+          const data = await readCard(cardId, signal)
+          setCardFormData({ 
+            id: data.id,
+            deckId: data.deckId, 
+            front: data.front, 
+            back: data.back })
+            console.log(cardFormData)
+        } catch (error) {
+          if (error.name !== "AbortError") {
+            console.error("Failed to read card", error)
+          }
+        }
+      }
+      retrieveCardData(cardId)
+      
+
+    }
+  }, [])
+
+
+
+
+
+
   const cardInfo = deckCards.find((card) => Number(cardId) === card.id);
   //handle submit for when save button is pushed
   const handleSubmit = (event) => {
@@ -16,9 +49,14 @@ const CardForm = ({ cardFormData, setCardFormData, foundDeck, intialCardFormData
     async function submitNewCard(newCard) {
       const controller = new AbortController();
       const { signal } = controller;
-      
+
       try {
-        createCard(deckId, newCard, signal)
+        if (!cardId) {
+          createCard(deckId, newCard, signal)
+        } else {
+          updateCard(newCard, signal)
+        }
+        
       } catch (error) {
         if (error.name !== 'AbortError') {
           console.error("Failed to create card", error)
@@ -26,19 +64,19 @@ const CardForm = ({ cardFormData, setCardFormData, foundDeck, intialCardFormData
       }
     }
     submitNewCard(cardFormData)
-    setCardFormData({...intialCardFormData})
+    setCardFormData({ ...intialCardFormData })
   }
 
   //handle canceling of both edit and create card
   const handleCancel = () => {
     setCardFormData(intialCardFormData)
-    navigate(`/decks/${deckId}`)   
+    navigate(`/decks/${deckId}`)
   }
 
   // handle change fuction to make sure form input is controlled 
-  const handleChange = ({target}) => {
-    setCardFormData({...cardFormData, [target.name]: target.value})
-    
+  const handleChange = ({ target }) => {
+    setCardFormData({ ...cardFormData, [target.name]: target.value })
+
   }
 
 
@@ -46,24 +84,25 @@ const CardForm = ({ cardFormData, setCardFormData, foundDeck, intialCardFormData
     <div className="card-form">
       <form onSubmit={handleSubmit}>
         <label htmlFor="front">Front</label>
-        <textarea 
-        type="text" 
-        id="front" 
-        name="front" 
-        onChange={handleChange}
-        value={cardFormData.front}
+        <textarea
+          type="text"
+          id="front"
+          name="front"
+          onChange={handleChange}
+          value={cardFormData.front}
         />
         <label htmlFor="back">Back</label>
-        <textarea 
-        type="text" 
-        id="back" 
-        name="back" 
-        onChange={handleChange}
-        value={cardFormData.back}
+        <textarea
+          type="text"
+          id="back"
+          name="back"
+          onChange={handleChange}
+          value={cardFormData.back}
         />
         <button onClick={handleCancel}>{cardId ? "Cancel" : "Done"}</button>
         <button type="submit">Save</button>
       </form>
+      <button onClick={() => console.log(cardFormData)}>Test</button>
     </div>
   )
 }
